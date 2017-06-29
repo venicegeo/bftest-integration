a4d127e4ddc977178bb0ca0b5f5ca6ab7f376bb9
jamesyarrington <jyarrington@vt.edu>
2017-06-29 09:39:09 -0400

    is variable defined

--- ci/beachfront_ua_tests_int.sh
+++ ci/beachfront_ua_tests_int.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "\\/ \\/ \\/ CHECK FOR ENV VARS HERE \\/ \\/ \\/"
+echo "$bf_username"
+echo "$sauce_user"
+echo "^  ^  ^  CHECK FOR ENV VARS HERE  ^  ^  ^"
+
 chmod 700 ./ci/Selenium/run_sel_tests.sh
 ./ci/Selenium/run_sel_tests.sh
