70095de08a9c8aae5c595226e7947e4f302d89fd
bpeizer <bpeizer@imagemattersllc.com>
2017-09-19 15:42:58 -0400

    Uploaded geopackage test

---
+++ ci/ets-gpkg12-bf/src/site/markdown/relnotes.md
@@ -0,0 +1,5 @@
+# Release Notes GeoPackage (gpkg) 1.2
+
+## 0.1 (2017-04-30)
+Initial release based on [1.0 test](https://github.com/opengeospatial/ets-gpkg10).
+ 
---
+++ ci/ets-gpkg12-bf/test.sh
@@ -0,0 +1,36 @@
+key='4bc81304-77f5-4ded-91f3-d2d087fffcfe'
+
+curl --user $key: -o test.gpkg -X GET \
+  https://bf-api.int.geointservices.io/v0/job/2ec7c319-19bb-48d7-b48a-ecd59ce12664.gpkg
+
+
+sleep 2s
+
+java -jar target/ets-gpkg12-0.2-SNAPSHOT-aio.jar --outputDir target src/main/config/test-run-props.xml
+
+sleep 2s
+
+filePath=$( ls target/testng)
+
+sleep 2s
+
+chmod 755 target/testng/$filePath/testng-results.xml
+
+fail=$(awk '/passed=/ { print $3}' target/testng/$filePath/testng-results.xml)
+
+pass=$(awk '/passed=/ { print $5}' target/testng/$filePath/testng-results.xml | tr -d '>')
+
+failComp='failed="0"'
+
+passComp='passed="0"'
+
+if [ "$pass" != "$passComp" ]  &&  [ "$fail" == "$failComp" ]; then
+echo "GeoPackage Passed Validation" 
+else
+echo "GeoPackage Failed Validation"
+exit 42;
+fi
+sleep 2s
+
+rm -rf target/testng
+rm -f test.gpkg
