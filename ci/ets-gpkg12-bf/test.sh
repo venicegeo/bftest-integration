d9f890210242ec174f203ff68f1334b3022a56f9
jamesyarrington <jyarrington@vt.edu>
2017-09-28 16:20:43 -0400

    check env var

--- ci/ets-gpkg12-bf/test.sh
+++ ci/ets-gpkg12-bf/test.sh
@@ -1,9 +1,11 @@
 key='4bc81304-77f5-4ded-91f3-d2d087fffcfe'
 
+echo $BF_GPKG_KEY
+
 curl --user $BF_GPKG_KEY: -o test.gpkg -X GET \
   https://bf-api.int.geointservices.io/v0/job/2ec7c319-19bb-48d7-b48a-ecd59ce12664.gpkg
 
-ls -l test.gkpg
+ls -l
 pwd
 
 sleep 2s
