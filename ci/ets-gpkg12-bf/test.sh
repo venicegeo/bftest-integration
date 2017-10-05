29fd26bb2762fc872233b57476a3574dccd093dd
bpeizer <bpeizer@imagemattersllc.com>
2017-10-05 15:28:58 -0400

    Updated Test
    Added dynamic pathing to test-run-props via script

--- ci/ets-gpkg12-bf/test.sh
+++ ci/ets-gpkg12-bf/test.sh
@@ -1,11 +1,29 @@
+#!/bin/sh
+
 key='4bc81304-77f5-4ded-91f3-d2d087fffcfe'
 
 echo $BF_GPKG_KEY
+
+rootPath=$(pwd)
+chmod a+r src/main/config/test-run-props.xml
+
+rm src/main/config/test-run-props.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n
+<!DOCTYPE properties SYSTEM \"http://java.sun.com/dtd/properties.dtd\">\n
+<properties version=\"1.0\">\n
+<comment>Test run arguments</comment>\n
+<entry key=\"iut\">file://$rootPath/test.gpkg</entry>\n
+<entry key=\"ics\">Core,Tiles,Features,Extensions,RTree Index,Metadata,Schema,CRS WKT,Tiled Gridded Elevation Data</entry>\n
+</properties>" >> src/main/config/test-run-props.xml
+
+
 
 curl --user $BF_GPKG_KEY: -o test.gpkg -X GET \
   https://bf-api.int.geointservices.io/v0/job/2ec7c319-19bb-48d7-b48a-ecd59ce12664.gpkg
   
 chmod a+r test.gpkg
+
 
 echo "CHECK DIR"
 ls -l
@@ -35,8 +53,13 @@ failComp='failed="0"'
 
 passComp='passed="0"'
 
+rootPath= pwd
+
 echo "TEST RESULTS"
 cat target/testng/$filePath/testng-results.xml
+
+rm -rf target/testng
+rm -f test.gpkg
 
 if [ "$pass" != "$passComp" ]  &&  [ "$fail" == "$failComp" ]; then
 echo "GeoPackage Passed Validation" 
@@ -46,5 +69,4 @@ exit 42;
 fi
 sleep 2s
 
-rm -rf target/testng
-rm -f test.gpkg
+
