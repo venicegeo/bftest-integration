e31c6eaca344b676103bd16cef505827fb18a36e
Patrick D <patrick.doody@rgi-corp.com>
2018-08-01 11:29:59 -0400

    Random job each request

--- ci/Locust/download.py
+++ ci/Locust/download.py
@@ -4,35 +4,42 @@ import EV
 
 # Behavior that simulates the repeated download of Jobs for a user.
 class DownloadBehavior(TaskSet):
-    # Obtain the Job reference to download.
+
+    # Obtain the Job List for the chosen API Key
     def on_start(self):
         # Get the complete list of Jobs for a user.
         with self.client.get("/job", auth = (EV.BF_API_KEY, ""), catch_response=True) as response:
-	    # Ensure the list has jobs.
-	    if len(response.json()["jobs"]["features"]) == 0:
-		print("API Key has no associated jobs. Test cannot continue.")
-		return
-            # Pick a random job from the list.
-            job = random.choice(list(response.json()["jobs"]["features"]))
-            # Extract the job's status from the response.
-            if job["properties"]["status"] == "Success":
-                self.job_id = job["properties"]["job_id"]
-            else:
-		print("Job was not a success. Skipping.")
-		return
+            self.jobs_list = response.json()
 
+    # Downloads a random Job from the list
     @task(1)
     def download(self):
-	data_type = random.choice(["gpkg", "geojson", "shp.zip"])
-        download_url = "/job/%s.%s" % (self.job_id, data_type)
+        # Get a random Job ID
+        job_id = self.get_random_job()
+        if job_id is None:
+            print("Job %s was not a Success. Skipping." % job_id)
+            return
+        # Choose a random Data Type
+        data_type = random.choice(["gpkg", "geojson", "shp.zip"])
+        download_url = "/job/%s.%s" % (job_id, data_type)
         print("Downloading Data %s" % download_url)
-	with self.client.get(download_url, auth = (EV.BF_API_KEY, ""), catch_response = True) as response:
-	    if response.ok:
-		print("Job %s length was %s" % (self.job_id, response.headers["Content-Length"]))
-		response.success()
-	    else:
-		response.failure()
+        # Download
+        with self.client.get(download_url, auth = (EV.BF_API_KEY, ""), catch_response = True) as response:
+            # Check HTTP Status
+            if response.ok:
+                print("Job %s length was %s" % (job_id, response.headers["Content-Length"]))
+                response.success()
+            else:
+                response.failure()
 
+    # Pull a random Job from the Jobs list.
+    def get_random_job(self):
+        job = random.choice(list(self.jobs_list["jobs"]["features"]))
+        # Extract the job's status from the response.
+        if job["properties"]["status"] == "Success":
+            return job["properties"]["job_id"]
+        else:
+            return None
 
 # Required for running a locust test.  Defines the user properites.
 class BeachfrontUser(HttpLocust):
@@ -40,4 +47,4 @@ class BeachfrontUser(HttpLocust):
 
     # Wait times between requests
     min_wait = 1000
-    max_wait = 5000
+    max_wait = 3000
