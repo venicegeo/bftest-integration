891f16a980cb746652a9ea82e550812792cc2bc6
go0ty <true.early@gmail.com>
2018-08-01 11:01:16 -0400

    Simplify Download Script

--- ci/Locust/download.py
+++ ci/Locust/download.py
@@ -7,33 +7,34 @@ class DownloadBehavior(TaskSet):
     # Obtain the Job reference to download.
     def on_start(self):
         # Get the complete list of Jobs for a user.
-        with self.client.get("/job", auth = (EV.BF_API_KEY, "")) as response:
+        with self.client.get("/job", auth = (EV.BF_API_KEY, ""), catch_response=True) as response:
+	    # Ensure the list has jobs.
+	    if len(response.json()["jobs"]["features"]) == 0:
+		print("API Key has no associated jobs. Test cannot continue.")
+		self.job_id = None
+		return
             # Pick a random job from the list.
-            rand_job = random.randint(0, len(response.json()["job"])-1)
+            job = random.choice(list(response.json()["jobs"]["features"]))
             # Extract the job's status from the response.
-            job = response.json()["job"][rand_job]
             if job["properties"]["status"] == "Success":
                 self.job_id = job["properties"]["job_id"]
             else:
-                self.interrupt()
+		return
 
     @task(1)
-    def download_geopackage(self):
-        download_url = "/job/%s.gpkg" % self.job_id
-        print("Downloading GeoPackage at %s" % download_url)
-        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
-
-    @task(1)
-    def download_shapefile(self):
-        download_url = "/job/%s.shp.zip" % self.job_id
-        print("Downloading Shapefile at %s" % download_url)
-        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
-
-    @task(1)
-    def download_geojson(self):
-        download_url = "/job/%s.geojson" % self.job_id
-        print("Downloading GeoJson at %s" % download_url)
-        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
+    def download(self):
+	if self.job_id is None:
+	    print("Task has no Job ID. Test cannot continue.")
+	    return
+	data_type = random.choice(["gpkg", "geojson", "shp.zip"])
+        download_url = "/job/%s.%s" % self.job_id, data_type
+        print("Downloading Data %s" % download_url)
+	with self.client.get(download_url, auth = (EV.BF_API_KEY, ""), catch_response = True) as response:
+	    if response.ok:
+		print("Job %s length was %s", self.job_id, response.headers["Content-Length"])
+		response.success()
+	    else:
+		response.failure()
 
 
 # Required for running a locust test.  Defines the user properites.
