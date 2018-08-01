2a5ad365bc49f52a413f7e485dd8a2d2f64fd437
Patrick D <patrick.doody@rgi-corp.com>
2018-08-01 10:10:29 -0400

    Add Download Stress Test

---
+++ ci/Locust/download.py
@@ -0,0 +1,48 @@
+from locust import HttpLocust, TaskSet, task
+
+import random
+
+# Environment Variables
+import EV
+
+# Behavior that simulates the repeated download of Jobs for a user.
+class DownloadBehavior(TaskSet):
+    # Obtain the Job reference to download.
+    def on_start(self):
+        # Get the complete list of Jobs for a user.
+        with self.client.get("/job", auth = (EV.BF_API_KEY, "")) as response:
+            # Pick a random job from the list.
+            rand_job = random.randint(0, len(response.json()["job"])-1)
+            # Extract the job's status from the response.
+            job = response.json()["job"][rand_job]
+            if job["properties"]["status"] == "Success":
+                self.job_id = job["properties"]["job_id"]
+            else:
+                self.interrupt()
+
+    @task(1)
+    def download_geopackage(self):
+        download_url = "/job/%s.gpkg" % self.job_id
+        print("Downloading GeoPackage at %s" download_url)
+        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
+
+    @task(1)
+    def download_shapefile(self):
+        download_url = "/job/%s.shp.zip" % self.job_id
+        print("Downloading Shapefile at %s" download_url)
+        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
+
+    @task(1)
+    def download_geojson(self):
+        download_url = "/job/%s.geojson" % self.job_id
+        print("Downloading GeoJson at %s" download_url)
+        self.client.get(download_url, auth = (EV.BF_API_KEY, ""))
+
+
+# Required for running a locust test.  Defines the user properites.
+class BeachfrontUser(HttpLocust):
+    task_set = DownloadBehavior
+
+    # Wait times between requests
+    min_wait = 1000
+    max_wait = 5000
